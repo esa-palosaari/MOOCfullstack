8e045a3fac83bb4f36038a7dd5f9193fd2491646
Esa Palosaari <esa.palosaari@gmail.com>
2019-06-26 12:05:14 +0300

    Unicafe askel 3 valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({neutral, bad, good, all}) => {
+    return (
+    <div>
+        <h2>Statistics</h2>
+        <p> Good: {good}<br />
+            Neutral: {neutral}<br />
+            Bad: {bad} <br /> <br />
+            All: {all} <br />
+            Average: {(good-bad) / all} <br />
+            Positive: {good/all * 100} %
+        </p>
+    </div>
+    )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -15,14 +29,7 @@ const App = () => {
         <button onClick={() => setGood(good+1)}>good</button>
         <button onClick={() => setNeutral(neutral+1)}>neutral</button>
         <button onClick={() => setBad(bad+1)}>bad</button>
-        <h2>Statistics</h2>
-        <p> Good: {good}<br />
-            Neutral: {neutral}<br />
-            Bad: {bad} <br />
-            All: {all} <br />
-            Average: {(good-bad) / all} <br />
-            Positive: {good/all * 100} %
-        </p>
+        <Statistics neutral={neutral} bad={ bad} good={good} all={all}/>
     </div>
   )
 }
