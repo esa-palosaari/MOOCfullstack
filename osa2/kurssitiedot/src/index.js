6d84591e81edeb3c2030cb83ec394c06e967c685
Esa Palosaari <esa.palosaari@gmail.com>
2019-06-27 16:22:54 +0300

    kurssitiedot Course moduuliksi

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,59 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './components/Course'
 
-const Header = ({course}) => {
-    console.log('header props', course)
-    return (
-        <>
-            <h2>{course.name}</h2>
-        </>
-    )
-}
-
-const Content = ({course}) => {
-    console.log('content props', course)
-    const rows = () =>
-    course.parts.map(mappedPart => 
-        <Part key={mappedPart.id} part={mappedPart.name} exercises={mappedPart.exercises} />)
-    return (
-        <>
-            {rows()}   
-        </>
-    )
-}
-
-const Part = (props) => {
-    console.log('Part props', props)
-    return (
-        <>
-            <p key={props.id}>{props.part} {props.exercises}</p>
-        </>
-    )
-}
-
-const Total = ({course}) => {
-    console.log('Total props', course)
-
-    const exerciseSum = () =>
-        course.parts.reduce((sum, current) => sum + current.exercises, 0)
-
-    return (
-        <>
-            <p><b>Number of exercises {exerciseSum()}</b></p>
-        </>
-    )
-}
-
-const Course = ({course}) => {
-    console.log('course props', course)
-    return (
-        <div>
-            <Header course={course}/>
-            <Content course={course} />
-            <Total course={course} />
-        </div>
-    )
-}
 
 
 const App = () => {
